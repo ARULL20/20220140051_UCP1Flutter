77f4aa56a26a49488d2525ff5f6b42620cfef831
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-27 18:09:09 +0700

    merubah sedikit pada main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 58, 152, 183)),
-      ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-        '/register' : (context) => const RegisterPage(),
-        
-
-      },
       debugShowCheckedModeBanner: false,
+      title: 'UCP 1 PAMLanjut',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const LoginPage(),
     );
   }
 }
