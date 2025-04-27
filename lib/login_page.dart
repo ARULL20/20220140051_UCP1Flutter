ef4f2cccf423831928769b9ec9cd943d4698e660
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-27 17:40:15 +0700

    membuat tampilan form login dengan validasi input email dan password

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,3 +16,60 @@ class _LoginPageState extends State<LoginPage> {
   bool _obscurePassword = true; // Untuk toggle password
   String? emailError;
   String? passwordError;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFEF7F4),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(flex: 2),
+            const Icon(Icons.flutter_dash, size: 100, color: Colors.black),
+            const SizedBox(height: 20),
+            const Text(
+              'SELAMAT DATANG KEMBALI',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 40),
+            _buildTextField(
+              label: 'Email',
+              icon: Icons.email,
+              controller: emailController,
+              errorText: emailError,
+            ),
+            const SizedBox(height: 20),
+            _buildTextField(
+              label: 'Password',
+              icon: Icons.lock,
+              isPassword: true,
+              controller: passwordController,
+              errorText: passwordError,
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF52FFDA),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  elevation: 4,
+                ),
+                onPressed: () async {
+                  setState(() {
+                    emailError = emailController.text.isEmpty ? 'Email tidak boleh kosong' : null;
+                    passwordError = passwordController.text.isEmpty ? 'Password tidak boleh kosong' : null;
+                  });
+
+                  if (emailError != null || passwordError != null) {
+                    return;
+                  }
