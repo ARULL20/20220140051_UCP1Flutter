08e102f65d2e4c56ceeb6778efb2c96ab58300ee
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-29 00:51:53 +0700

    untuk memastikan bahwa halaman selalu menerima data

--- lib/detailsuksespage.dart
+++ lib/detailsuksespage.dart
@@ -7,3 +7,13 @@ class DetailSuksesPage extends StatelessWidget {
   final int jumlahBarang;
   final int hargaSatuan;
   final int totalHarga;
+
+  const DetailSuksesPage({
+    Key? key,
+    required this.tanggal,
+    required this.jenisTransaksi,
+    required this.jenisBarang,
+    required this.jumlahBarang,
+    required this.hargaSatuan,
+    required this.totalHarga,
+  }) : super(key: key);
