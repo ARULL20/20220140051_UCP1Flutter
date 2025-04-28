7a7190981aa89da240d5dc23c653a97cf6726d95
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-29 00:50:59 +0700

    membuat class detailsukses dari pendataanbarang

---
+++ lib/detailsuksespage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DetailSuksesPage extends StatelessWidget {
+  final String tanggal;
+  final String jenisTransaksi;
+  final String jenisBarang;
+  final int jumlahBarang;
+  final int hargaSatuan;
+  final int totalHarga;
