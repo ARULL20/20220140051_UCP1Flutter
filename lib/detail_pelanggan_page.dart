f1294abc542217942e7cde49a468be4def70a5dc
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-29 00:14:49 +0700

    menambahkan tombol kembali,mengatur backround,avatar gambar,nama pelanggan dan yang di isi dalam page pelanggan

--- lib/detail_pelanggan_page.dart
+++ lib/detail_pelanggan_page.dart
@@ -17,3 +17,66 @@ class DetailPelangganPage extends StatelessWidget {
     required this.provinsi,
     required this.kodePos,
   }) : super(key: key);
+
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 3, 183, 219),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('Detail $nama'),
+      ),
+      backgroundColor: const Color(0xFFFFF5F2),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const SizedBox(height: 16),
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/profil2.jpg'), // ganti sesuai asset kamu
+            ),
+            const SizedBox(height: 16),
+            Text(
+              nama,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(email),
+            Text(noHp),
+            const SizedBox(height: 24),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Alamat'),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: TextEditingController(text: alamat),
+                  readOnly: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Provinsi'),
+                          const SizedBox(height: 8),
+                          TextField(
+                            controller: TextEditingController(text: provinsi),
+                            readOnly: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
