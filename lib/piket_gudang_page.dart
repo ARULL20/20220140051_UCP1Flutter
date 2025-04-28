d089fb39d12b6072d0b13ac4e73ad85a76bb3bfc
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-28 21:31:04 +0700

    membuat halaman piket gudang

---
+++ lib/piket_gudang_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PiketGudangPage extends StatefulWidget {
+  const PiketGudangPage({Key? key}) : super(key: key);
+
+  @override
+  State<PiketGudangPage> createState() => _PiketGudangPageState();
+}
