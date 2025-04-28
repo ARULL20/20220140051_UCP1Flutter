d1ab9d77d162c997a473ca9f98c7addc3ac735bc
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-28 23:59:14 +0700

    untuk mengelola form input berisi nama,email,no hp,alamat,provinsi,dan kode pos

--- lib/data_pelanggan_page.dart
+++ lib/data_pelanggan_page.dart
@@ -7,3 +7,20 @@ class DataPelangganPage extends StatefulWidget {
   @override
   State<DataPelangganPage> createState() => _DataPelangganPageState();
 }
+
+class _DataPelangganPageState extends State<DataPelangganPage> {
+  final TextEditingController namaController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController noHpController = TextEditingController();
+  final TextEditingController alamatController = TextEditingController();
+  final TextEditingController provinsiController = TextEditingController();
+  final TextEditingController kodePosController = TextEditingController();
+
+  String? namaError;
+  String? emailError;
+  String? noHpError;
+  String? alamatError;
+  String? provinsiError;
+  String? kodePosError;
+
+  final primaryColor = Color.fromARGB(255, 3, 183, 219);
