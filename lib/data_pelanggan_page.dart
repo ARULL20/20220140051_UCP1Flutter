1363c4532cb9a7d69c5330c0fd559a54b9766577
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-28 23:57:13 +0700

    membuat class data pelanggan

---
+++ lib/data_pelanggan_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'detail_pelanggan_page.dart'; // <--- Tambahkan import halaman detail di sini
+
+class DataPelangganPage extends StatefulWidget {
+  const DataPelangganPage({Key? key}) : super(key: key);
+
+  @override
+  State<DataPelangganPage> createState() => _DataPelangganPageState();
+}
