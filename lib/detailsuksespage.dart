4dc92a68ea9fed991a3a97b6151e99aaba083c95
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-04-29 00:53:16 +0700

    backroundcolor, padding dan column text untuk judul, tombol selesai

--- lib/detailsuksespage.dart
+++ lib/detailsuksespage.dart
@@ -17,3 +17,48 @@ class DetailSuksesPage extends StatelessWidget {
     required this.hargaSatuan,
     required this.totalHarga,
   }) : super(key: key);
+
+@override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 249, 247),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.check_circle, color: Colors.green, size: 100),
+            const SizedBox(height: 20),
+            const Text(
+              'Data Berhasil Disimpan',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            buildRow('Tanggal', tanggal),
+            buildRow('Jenis Transaksi', jenisTransaksi),
+            buildRow('Jenis Barang', jenisBarang),
+            buildRow('Jumlah Barang', jumlahBarang.toString()),
+            buildRow('Jenis Harga Satuan', 'Rp. ${hargaSatuan.toString()}'),
+            buildRow('Total Harga', 'Rp. ${totalHarga.toString()}'),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 0, 247, 4),
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Selesai', style: TextStyle(fontSize: 16)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
